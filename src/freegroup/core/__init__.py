4739f84e85d4311ac671200fb0966c5307ed1154
Kirill Brilliantov <ki.br178@gmail.com>
2022-12-01 23:02:26 +0000

    Add to __init__.py

--- src/freegroup/core/__init__.py
+++ src/freegroup/core/__init__.py
@@ -1,3 +1,3 @@
 from .core import Word, to_numpy, to_str, reciprocal, \
     commutator, conjugation, iterable_normal_closure_embedding, normal_closure_embedding, \
-    normalize, iterable_commutator
+    normalize, iterable_commutator, is_from_normal_closure
